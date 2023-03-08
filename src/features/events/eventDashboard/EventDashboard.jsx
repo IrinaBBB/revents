e43fdea8ebacd855a0adc6972374b6270f4897cb
IrinaBBB <balkarovairina@gmail.com>
2023-03-08 16:07:04 +0100

    End of section 4

---
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import {Grid} from "semantic-ui-react";
+import EventList from "./EventList";
+import EventForm from "../eventForm/EventForm";
+import {sampleData} from "../../../app/api/sampleData";
+
+export default function EventDashboard({formOpen, setFormOpen, selectEvent, selectedEvent}) {
+    const [events, setEvents] = useState(sampleData);
+
+    function handleCreateEvent(event) {
+        setEvents([...events, event]);
+    }
+
+    function handleUpdateEvent(updatedEvent) {
+        setEvents(events.map(event => event.id === updatedEvent.id ? updatedEvent : event));
+        selectEvent(null);
+    }
+
+    function handleDeleteEvent(eventId) {
+        setEvents(events.filter(event => event.id !== eventId));
+    }
+
+    return (
+        <Grid>
+            <Grid.Column width={10}>
+                <EventList events={events} selectEvent={selectEvent} deleteEvent={handleDeleteEvent}/>
+            </Grid.Column>
+            <Grid.Column width={6}>
+                {formOpen &&
+                    <EventForm setFormOpen={setFormOpen} setEvents={setEvents} createEvent={handleCreateEvent}
+                               selectedEvent={selectedEvent} key={selectedEvent ? selectedEvent.id : null}
+                               updateEvent={handleUpdateEvent}/>
+                }
+            </Grid.Column>
+        </Grid>
+    );
+}
+
+
---
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {Button, Icon, Item, List, Segment} from "semantic-ui-react";
+import EventListAttendee from "./EventListAttendee";
+
+export default function EventListItem({event, selectEvent, deleteEvent}) {
+    return (
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src={event.hostPhotoURL}/>
+                        <Item.Content>
+                            <Item.Header content={event.title}/>
+                            <Item.Description>
+                                Hosted by {event.hostedBy}
+                            </Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <Icon name='clock'/> {event.date}
+                <Icon name='marker'/> {event.venue}
+            </Segment>
+            <Segment secondary>
+                <List horizontal>
+                    {event.attendees.map(attendee => (
+                        <EventListAttendee attendee={attendee} key={attendee.id}/>
+                    ))}
+                </List>
+            </Segment>
+            <Segment clearing>
+                <div>{event.description}</div>
+                <Button onClick={(e) => selectEvent(event)} color='teal' floated='right' content='View'/>
+                <Button onClick={(e) => deleteEvent(event.id)} color='red' floated='right' content='Delete'/>
+            </Segment>
+        </Segment.Group>
+    );
+}

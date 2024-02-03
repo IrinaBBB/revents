1bdbe689766a968bbd659965c50f876404d17b1d
IrinaB <balkarovairina@gmail.com>
2024-02-03 09:26:27 +0100

    Add EventDashboard Component

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,12 +1,11 @@
-import { Button } from 'semantic-ui-react'
+import EventDashboard from '../../features/events/dashboard/EventDashboard.tsx'
 
 function App() {
 
     return (
         <>
             <h1>Welcome to Revents</h1>
-            <button className='ui icon red button'><i className='user icon'></i></button>
-            <Button icon='smile' content='Button' color='green' loading={false}/>
+            <EventDashboard />
         </>
     )
 }

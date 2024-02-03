27dd2606ac7ece5e7ba633cb332a7f2cb2f75499
IrinaB <balkarovairina@gmail.com>
2024-02-03 09:15:34 +0100

    Add Semantic UI
    Used specific version of semantic-ui-react (semantic-react-ui@3.0.0-beta.0")

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from 'semantic-ui-react'
+
 function App() {
 
     return (
-        <h1>
-            Welcome to Revents
-        </h1>
+        <>
+            <h1>Welcome to Revents</h1>
+            <button className='ui icon red button'><i className='user icon'></i></button>
+            <Button icon='smile' content='Button' color='green' loading={false}/>
+        </>
     )
 }
 

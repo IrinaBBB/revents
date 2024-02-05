84f07a38b609d7301da2ee244e27fe61b7f75d7b
IrinaB <balkarovairina@gmail.com>
2024-02-05 11:43:29 +0100

    Initialize Firestore

---
+++ src/app/config/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import 'firebase/firesore'
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: 'revents-2023-6772b.firebaseapp.com',
+    projectId: 'revents-2023-6772b',
+    storageBucket: 'revents-2023-6772b.appspot.com',
+    messagingSenderId: '652549682034',
+    appId: '1:652549682034:web:a93817436813b4fdb363eb',
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
